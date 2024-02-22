f99850e5e611b0d9fdfb4d3c86056e6aaa3703bf
iEncode Tech Team <iencodetech@gmail.com>
2024-02-22 22:53:22 +0530

    update alert box

--- src/components/AlertBox.js
+++ src/components/AlertBox.js
@@ -4,25 +4,25 @@ function GetAlert({ alert, message }) {
     switch (alert) {
         case 'danger':
             return (<div className="alert alert-danger alert-dismissible">
-                <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button>
+                {/* <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button> */}
                 <h5><i className="icon fas fa-ban"></i> Alert!</h5>
                 {message}
             </div>);
         case 'info':
             return (<div className="alert alert-info alert-dismissible">
-                <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button>
+                {/* <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button> */}
                 <h5><i className="icon fas fa-info"></i> Alert!</h5>
                 {message}
             </div>);
         case 'warning':
             return (<div className="alert alert-warning alert-dismissible">
-                <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button>
+                {/* <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button> */}
                 <h5><i className="icon fas fa-exclamation-triangle"></i> Alert!</h5>
                 {message}
             </div>);
         case 'success':
             return (<div className="alert alert-success alert-dismissible">
-                <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button>
+                {/* <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button> */}
                 <h5><i className="icon fas fa-check"></i> Alert!</h5>
                 {message}
             </div>);
